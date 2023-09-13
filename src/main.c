14859ea04c64e15139ad70a8000ac2db232ef9d2
Leon Windhorst <leon@windhorst.dev>
2023-09-13 13:59:38 +0200

    chore: run clang-format

--- src/main.c
+++ src/main.c
@@ -8,97 +8,93 @@
  *
  */
 
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 
 typedef uint32_t u32;
 typedef float f32;
 
-#define as_u32(x)   (*(u32 *)&x)
+#define as_u32(x) (*(u32 *)&x)
 #define as_f32(x) (*(f32 *)&x)
-
-
 
 // Util stuff
 
 void print_bits(u32 x) {
-    u32 len = sizeof(u32) * 8;
-    for (u32 i = 0; i < len; ++i) {
-        printf("%c", x & (1 << (len - i - 1)) ? '1' : '0');
-    }
+  u32 len = sizeof(u32) * 8;
+  for (u32 i = 0; i < len; ++i) {
+    printf("%c", x & (1 << (len - i - 1)) ? '1' : '0');
+  }
 }
 
 void print_bits_examples() {
-    for (f32 x = 1.0f; x < 100.0f; x += 1.0f) {
-        printf("\n(binary)   ");
-        print_bits(as_u32(x));
-        printf("\n(f32)    %f", x);
-        printf("\n(int)      %d\n", as_u32(x));
-    }
-
-    u32 x = 0x5F3759DF;
-    printf("\nQuake III 'magic constant' 0x5F3759DF");
+  for (f32 x = 1.0f; x < 100.0f; x += 1.0f) {
     printf("\n(binary)   ");
-    print_bits(x);
-    printf("\n(f32)    %f", as_f32(x));
-    printf("\n(int)      %d\n", x);
+    print_bits(as_u32(x));
+    printf("\n(f32)    %f", x);
+    printf("\n(int)      %d\n", as_u32(x));
+  }
+
+  u32 x = 0x5F3759DF;
+  printf("\nQuake III 'magic constant' 0x5F3759DF");
+  printf("\n(binary)   ");
+  print_bits(x);
+  printf("\n(f32)    %f", as_f32(x));
+  printf("\n(int)      %d\n", x);
 }
-
-
 
 // From the paper
 
 f32 frsr_quake(f32 x) {
-    printf("\nFRSR Quake");
+  printf("\nFRSR Quake");
 
-    const u32 C = 0x5F3759DF;
+  const u32 C = 0x5F3759DF;
 
-    u32 X = as_u32(x);
-    u32 Y = C - (X >> 1);
-    f32 y = as_f32(Y);
-    f32 result = y * (1.5f - 0.5f * x * y * y);
+  u32 X = as_u32(x);
+  u32 Y = C - (X >> 1);
+  f32 y = as_f32(Y);
+  f32 result = y * (1.5f - 0.5f * x * y * y);
 
-    printf("\n(x)             %f", x);
-    printf("\n(1 / sqrt(x))   %f\n", result);
+  printf("\n(x)             %f", x);
+  printf("\n(1 / sqrt(x))   %f\n", result);
 
-    return result;
+  return result;
 }
 
 f32 frsr_mon_0(f32 x) {
-    printf("\nFRSR degree-0 monic");
+  printf("\nFRSR degree-0 monic");
 
-    const u32 C = 0x5F37642F;
+  const u32 C = 0x5F37642F;
 
-    u32 X = as_u32(x);
-    u32 Y = C - (X >> 1);
-    f32 result = as_f32(Y);
+  u32 X = as_u32(x);
+  u32 Y = C - (X >> 1);
+  f32 result = as_f32(Y);
 
-    printf("\n(x)             %f", x);
-    printf("\n(1 / sqrt(x))   %f\n", result);
+  printf("\n(x)             %f", x);
+  printf("\n(1 / sqrt(x))   %f\n", result);
 
-    return result;
+  return result;
 }
 
 f32 frsr_deg_0(f32 x) {
-    printf("\nFRSR degree-0 polynomial");
+  printf("\nFRSR degree-0 polynomial");
 
-    const u32 C = 0xBEBFFDAA;
+  const u32 C = 0xBEBFFDAA;
 
-    u32 X = as_u32(x);
-    u32 Y = (C - X) >> 1;
-    f32 y = as_f32(Y);
-    f32 result = y * 0.79247999f;
+  u32 X = as_u32(x);
+  u32 Y = (C - X) >> 1;
+  f32 y = as_f32(Y);
+  f32 result = y * 0.79247999f;
 
-    printf("\n(x)             %f", x);
-    printf("\n(1 / sqrt(x))   %f\n", result);
+  printf("\n(x)             %f", x);
+  printf("\n(1 / sqrt(x))   %f\n", result);
 
-    return result;
+  return result;
 }
 
 int main(void) {
-    frsr_quake(16.0f);
-    frsr_mon_0(16.0f);
-    frsr_deg_0(16.0f);
+  frsr_quake(16.0f);
+  frsr_mon_0(16.0f);
+  frsr_deg_0(16.0f);
 
-    return 0;
+  return 0;
 }
